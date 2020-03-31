5e0ae2faf75ef63f4c3889dfa6283f9e40b39677
sid-thakur47 <sidthakur258@gmail.com>
2020-03-31 15:16:55 +0530

    ADDED:ArithematicNSorting To compute multiply and add

--- ArithematicNSorting.sh
+++ ArithematicNSorting.sh
@@ -3,4 +3,5 @@ read a
 read b
 read c
 resultExpr1=$(($(($a+$b))*$c))
-echo "Result of Expression:"$resultExpr1
+resultExpr2=$(($(($a*$b))+$c))
+echo -e "Result of Expression- \n1 is $resultExpr1 \n2 is $resultExpr2"
