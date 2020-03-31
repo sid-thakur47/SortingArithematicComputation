b60eb760495c86a0525256c18def1a246efbdd8d
sid-thakur47 <sidthakur258@gmail.com>
2020-03-31 17:08:48 +0530

    ADDED:ArithematicNSorting Read values from Dictinory to array

--- ArithematicNSorting.sh
+++ ArithematicNSorting.sh
@@ -1,4 +1,5 @@
 declare -A exprDict
+declare -a convertedInArray
 
 echo "Enter three numbers:"
 read a
@@ -6,19 +7,26 @@ read b
 read c
 
 function getComputation() {
-	resultExpr1=$(($(($a+$b))*$c))
-	resultExpr2=$(($(($a*$b))+$c))
-	resultExpr3=$(($(($c+$a))/$b))
-	resultExpr4=$(($(($a%$b))+$c))
+		resultExpr1=$(($(($a+$b))*$c))
+		resultExpr2=$(($(($a*$b))+$c))
+		resultExpr3=$(($(($c+$a))/$b))
+		resultExpr4=$(($(($a%$b))+$c))
 	echo -e "Result of:\n1 is $resultExpr1\n2 is $resultExpr2\n3 is $resultExpr3\n4 is $resultExpr4"
 }
 
 function storeToDictionry() {
-	getComputation
 		exprDict[expression1]=$resultExpr1
 		exprDict[expression2]=$resultExpr2
 		exprDict[expression3]=$resultExpr3
 		exprDict[expression4]=$resultExpr4
 	echo "Results of ${!exprDict[*]} are:"${exprDict[*]}
 }
+
+function dictionryToArray() {
+		convertedArray=("${exprDict[*]}")
+	echo "Converted Array:" ${convertedArray[*]}
+}
+getComputation
 storeToDictionry
+dictionryToArray
+
