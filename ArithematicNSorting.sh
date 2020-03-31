56c44f7752c397f56f3276131f01851ed7fdd1b5
sid-thakur47 <sidthakur258@gmail.com>
2020-03-31 15:44:48 +0530

    ADDED:ArithematicNSorting To compute Add and Divide

--- ArithematicNSorting.sh
+++ ArithematicNSorting.sh
@@ -4,4 +4,5 @@ read b
 read c
 resultExpr1=$(($(($a+$b))*$c))
 resultExpr2=$(($(($a*$b))+$c))
-echo -e "Result of Expression- \n1 is $resultExpr1 \n2 is $resultExpr2"
+resultExpr3=$(($(($c+$a))/$b))
+echo -e "Result of :\n1 is $resultExpr1\n2 is $resultExpr2\n3 is $resultExpr3"
