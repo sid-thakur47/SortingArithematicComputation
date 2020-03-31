8b8e962ccecf81c23412191269749854a106bb4a
sid-thakur47 <sidthakur258@gmail.com>
2020-03-31 16:30:31 +0530

    ADDED:ArithematicNSorting Store expression in dictionary

--- ArithematicNSorting.sh
+++ ArithematicNSorting.sh
@@ -1,11 +1,24 @@
+declare -A exprDict
+
 echo "Enter three numbers:"
 read a
 read b
 read c
 
-resultExpr1=$(($(($a+$b))*$c))
-resultExpr2=$(($(($a*$b))+$c))
-resultExpr3=$(($(($c+$a))/$b))
-resultExpr4=$(($(($a%$b))+$c))
+function getComputation() {
+	resultExpr1=$(($(($a+$b))*$c))
+	resultExpr2=$(($(($a*$b))+$c))
+	resultExpr3=$(($(($c+$a))/$b))
+	resultExpr4=$(($(($a%$b))+$c))
+	echo -e "Result of:\n1 is $resultExpr1\n2 is $resultExpr2\n3 is $resultExpr3\n4 is $resultExpr4"
+}
 
-echo -e "Result of :\n1 is $resultExpr1\n2 is $resultExpr2\n3 is $resultExpr3\n4 is $resultExpr4"
+function storeToDictionry() {
+	getComputation
+		exprDict[expression1]=$resultExpr1
+		exprDict[expression2]=$resultExpr2
+		exprDict[expression3]=$resultExpr3
+		exprDict[expression4]=$resultExpr4
+	echo "Results of ${!exprDict[*]} are:"${exprDict[*]}
+}
+storeToDictionry
