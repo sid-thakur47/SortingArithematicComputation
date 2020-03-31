28fc63c94c2aad7bce4641bb4d428a7c5476a01c
sid-thakur47 <sidthakur258@gmail.com>
2020-03-31 21:40:42 +0530

    MODIFIED:ArithematicNSorting changed expression

--- ArithematicNSorting.sh
+++ ArithematicNSorting.sh
@@ -2,7 +2,7 @@ echo "Enter three numbers:"
 read a
 read b
 read c
-resultExpr1=$(($(($a+$b))*$c))
+resultExpr1=$(($a+$(($b*$c))))
 resultExpr2=$(($(($a*$b))+$c))
-resultExpr3=$(($(($c+$a))/$b))
-echo -e "Result of :\n1 is $resultExpr1\n2 is $resultExpr2\n3 is $resultExpr3"
+resultExpr3=$(($c+$(($a/$b))))
+echo -e "Result of Expression- \n1 is $resultExpr1 \n2 is $resultExpr2  \n3 is $resultExpr3"
