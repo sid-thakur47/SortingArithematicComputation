2eccac6db7044598da1ec1a3cd1b0e5d194758fa
sid-thakur47 <sidthakur258@gmail.com>
2020-03-31 21:49:35 +0530

    MODIFIED:ArithematicNSorting changed expression

--- ArithematicNSorting.sh
+++ ArithematicNSorting.sh
@@ -6,9 +6,9 @@ read b
 read c
 
 function getComputation() {
-	resultExpr1=$(($(($a+$b))*$c))
+	resultExpr1=$(($a+$(($b*$c))))
 	resultExpr2=$(($(($a*$b))+$c))
-	resultExpr3=$(($(($c+$a))/$b))
+	resultExpr3=$(($c+$(($a/$b))))
 	resultExpr4=$(($(($a%$b))+$c))
 	echo -e "Result of:\n1 is $resultExpr1\n2 is $resultExpr2\n3 is $resultExpr3\n4 is $resultExpr4"
 }
