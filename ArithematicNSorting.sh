2e6a233cce36003adc7135a46f49bfa8e00c94f9
sid-thakur47 <sidthakur258@gmail.com>
2020-03-31 16:01:31 +0530

    ADDED:ArithematicNSorting To compute Mod and Add

--- ArithematicNSorting.sh
+++ ArithematicNSorting.sh
@@ -2,7 +2,10 @@ echo "Enter three numbers:"
 read a
 read b
 read c
+
 resultExpr1=$(($(($a+$b))*$c))
 resultExpr2=$(($(($a*$b))+$c))
 resultExpr3=$(($(($c+$a))/$b))
-echo -e "Result of :\n1 is $resultExpr1\n2 is $resultExpr2\n3 is $resultExpr3"
+resultExpr4=$(($(($a%$b))+$c))
+
+echo -e "Result of :\n1 is $resultExpr1\n2 is $resultExpr2\n3 is $resultExpr3\n4 is $resultExpr4"
