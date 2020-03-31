00f23194deb0b01f7f7565c5e3bc36f51633697e
sid-thakur47 <62283575+sid-thakur47@users.noreply.github.com>
2020-03-31 21:06:48 +0530

    MODIFIED: ArithematicNSorting changed expression

--- ArithematicNSorting.sh
+++ ArithematicNSorting.sh
@@ -2,6 +2,6 @@ echo "Enter three numbers:"
 read a
 read b
 read c
-resultExpr1=$(($(($a+$b))*$c))
+resultExpr1=$(($a+$(($b*$c))))
 resultExpr2=$(($(($a*$b))+$c))
 echo -e "Result of Expression- \n1 is $resultExpr1 \n2 is $resultExpr2"
