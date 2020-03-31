fc1abb31d8aa4c727f30f548428741a331c57e2e
sid-thakur47 <sidthakur258@gmail.com>
2020-03-31 17:16:20 +0530

    ADDED:ArithematicNSorting Corrected Indentation

--- ArithematicNSorting.sh
+++ ArithematicNSorting.sh
@@ -7,18 +7,18 @@ read b
 read c
 
 function getComputation() {
-		resultExpr1=$(($(($a+$b))*$c))
-		resultExpr2=$(($(($a*$b))+$c))
-		resultExpr3=$(($(($c+$a))/$b))
-		resultExpr4=$(($(($a%$b))+$c))
+	resultExpr1=$(($(($a+$b))*$c))
+	resultExpr2=$(($(($a*$b))+$c))
+	resultExpr3=$(($(($c+$a))/$b))
+	resultExpr4=$(($(($a%$b))+$c))
 	echo -e "Result of:\n1 is $resultExpr1\n2 is $resultExpr2\n3 is $resultExpr3\n4 is $resultExpr4"
 }
 
 function storeToDictionry() {
-		exprDict[expression1]=$resultExpr1
-		exprDict[expression2]=$resultExpr2
-		exprDict[expression3]=$resultExpr3
-		exprDict[expression4]=$resultExpr4
+	exprDict[expression1]=$resultExpr1
+	exprDict[expression2]=$resultExpr2
+	exprDict[expression3]=$resultExpr3
+	exprDict[expression4]=$resultExpr4
 	echo "Results of ${!exprDict[*]} are:"${exprDict[*]}
 }
 
